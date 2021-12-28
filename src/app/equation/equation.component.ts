29b7d632fbdb4c096ad25b228c1762bb0d4c425a
Gustavonvp <gustavonvp@gmail.com>
2021-12-28 19:57:02 -0300

    first commit

---
+++ src/app/equation/equation.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrls: ['./equation.component.css']
+})
+export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl("")
+  })
+  
+  constructor() { }
+
+get a() {
+  return this.mathForm.value.a;
+}
+
+get b() {
+  return this.mathForm.value.b;
+}
+
+
+  ngOnInit() {
+  }
+
+  randomNumber(){
+    return Math.floor(Math.random() * 10);
+  }
+
+}
